3db33816298944e249b9a74401edfb4c3d760eca
Milky <theshiretherivendell@outlook.com>
2023-11-08 10:48:35 +0800

    fix(controllers, views): fix bugs relative path cause exe fail - update .gitignore: ignore json,csv,xlsx

--- main.py
+++ main.py
@@ -3,11 +3,22 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from utils.files2db import *
-
+from controllers.demo_backend import MainWindow
+import views
+import utils
+import models
+from PyQt5.QtWidgets import QApplication
+import sys,os
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    pdf_path=".\models\plan2021.pdf"
-    pdf2db(pdf_path)
+# if getattr(sys, 'frozen', False):
+#     working_dir = os.path.dirname(sys.executable)
+# elif __file__:
+#     working_dir = os.path.dirname(__file__)
+# os.chdir(working_dir)
+# print("working dir in main: "+working_dir)
+if __name__=='__main__':
+    app=QApplication([])
+    w=MainWindow()
+    app.exec_()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
